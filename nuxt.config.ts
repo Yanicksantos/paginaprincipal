3059bd1dd8168dc0bd656856723168172522068a
Yanick Santos <yanickdev@outlook.com>
2024-05-31 22:23:34 -0300

    configuração para SEO adicionadas 1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,28 +1,51 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr:true,
+  ssr: true,
   devtools: { enabled: false },
   sourcemap: true,
   imports: {
     autoImport: true,
     injectAtEnd: true,
   },
-
-  modules: ['vuetify-nuxt-module', '@vee-validate/nuxt',
-  ['@nuxtjs/google-fonts', {
-    families: {
-      Roboto: true,
-      Montserrat: true,
-     
-     
-    }
-  }],
-],
-
-vuetify: {
-  moduleOptions: {
-    prefixComposables: true,
+  modules: [
+    'vuetify-nuxt-module', 
+    '@vee-validate/nuxt',
+    ['@nuxtjs/google-fonts', {
+      families: {
+        Roboto: true,
+        Montserrat: true,
+      }
+    }],
+  ],
+  vuetify: {
+    moduleOptions: {
+      prefixComposables: true,
+    },
   },
-},
+  app: {
+    head: {
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1.0",
+      meta: [
+        { name: "description", content: "A Pred Engenharia é uma provedora de soluções para gestão de ativos e processos, com foco na garantia da segurança, confiabilidade e produtividade das atividades. Atuando na indústria e na área de serviços desde 1993, suas inovadoras soluções são fornecidas ao mercado através de três áreas de negócios: Serviços Técnicos, Tecnologia da Informação e Automação Industrial." },
+        { name: "keywords", content: "Inspeções, Preditiva, Manutenção, S3I, Sistema inteligente de Intervenção Industrial, SERVIÇOS TÉCNICOS DE INSPEÇÃO, MCI, Manutenção Condicionada a Inspeção" },
+        { name: "author", content: "Pred Engenharia" },
+        { property: "og:title", content: "Pred Engenharia" },
+        { property: "og:description", content: "A Pred Engenharia é uma provedora de soluções para gestão de ativos e processos, com foco na garantia da segurança, confiabilidade e produtividade das atividades. Atuando na indústria e na área de serviços desde 1993, suas inovadoras soluções são fornecidas ao mercado através de três áreas de negócios: Serviços Técnicos, Tecnologia da Informação e Automação Industrial." },
+        { property: "og:image", content: "https://pred-engenharia.vercel.app/30anos.webp" },
+        { property: "og:url", content: "https://pred-engenharia.vercel.app/" },
+        { property: "og:type", content: "website" },
+        { name: "twitter:card", content: "summary_large_image" },
+        { name: "twitter:title", content: "Pred Engenharia" },
+        { name: "twitter:description", content: "A Pred Engenharia é uma provedora de soluções para gestão de ativos e processos, com foco na garantia da segurança, confiabilidade e produtividade das atividades. Atuando na indústria e na área de serviços desde 1993, suas inovadoras soluções são fornecidas ao mercado através de três áreas de negócios: Serviços Técnicos, Tecnologia da Informação e Automação Industrial." },
+        { name: "twitter:image", content: "https://pred-engenharia.vercel.app/30anos.webp" },
 
-})
+      ],
+      link: [
+        { rel: "canonical", href: "https://pred-engenharia.vercel.app/" },
+        { rel: "stylesheet", href: "https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" }
+      ],
+      title: "Pred Engenharia"
+    }
+  }
+});
