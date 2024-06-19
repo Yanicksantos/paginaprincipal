7bd18a11148f4c72412ac5e56936293d7e8fcfe7
Yanick Santos <yanickdev@outlook.com>
2024-06-19 12:53:53 -0300

    tradução tela inspeção incluida

--- pages/servicos/inspecoes.vue
+++ pages/servicos/inspecoes.vue
@@ -1,171 +1,106 @@
 <template>
     <div>
-        <v-img
-        class="d-md-none "
+      <!-- Imagem para dispositivos móveis -->
+      <v-img
+        class="d-md-none"
         height="350"
         gradient="to top, rgba(0,0,0,.9), transparent"
         cover
         src="/services2.webp"
         dark
-        >
-            <div class=" d-flex flex-column justify-center align-center w-100 h-100 pa-4" style="background-image: linear-gradient(to top, rgba(0,0,0,.9), transparent);">
-                
-                <div><v-icon icon="mdi-account-search" size="50"></v-icon></div>
-                <h1 class="text-grey-lighten-1 text-h5 text-center font-weight-bold elevation-24 mt-2 pa-2">SERVIÇOS TÉCNICOS DE INSPEÇÃO</h1>           
-            
-            </div>
-        </v-img>
-
-        <!-- desktop -->
-
-        <v-img
+      >
+        <div class="d-flex flex-column justify-center align-center w-100 h-100 pa-4" style="background-image: linear-gradient(to top, rgba(0,0,0,.9), transparent);">
+          <div><v-icon icon="mdi-account-search" size="50"></v-icon></div>
+          <h1 class="text-grey-lighten-1 text-h5 text-center font-weight-bold elevation-24 mt-2 pa-2">{{ $t('main_content1') }}</h1>
+        </div>
+      </v-img>
+  
+      <!-- Imagem para desktops -->
+      <v-img
         class="d-none d-md-flex mt-14"
         gradient="to top, rgba(0,0,0,.9), transparent"
         height="600"
         cover
         src="/services2.webp"
         dark
-        >
-            <div class=" d-flex flex-column justify-center align-center w-100 h-100 pa-4" style="background-image: linear-gradient(to top, rgba(0,0,0,.9), transparent);">
-                <div><v-icon icon="mdi-account-search" size="50"></v-icon></div>
-                <h1 class="text-grey-lighten-1 text-h5 text-md-h2 text-center font-weight-bold elevation-24 mt-2 pa-4">SERVIÇOS TÉCNICOS DE INSPEÇÃO</h1>
+      >
+        <div class="d-flex flex-column justify-center align-center w-100 h-100 pa-4" style="background-image: linear-gradient(to top, rgba(0,0,0,.9), transparent);">
+          <div><v-icon icon="mdi-account-search" size="50"></v-icon></div>
+          <h1 class="text-grey-lighten-1 text-h5 text-md-h2 text-center font-weight-bold elevation-24 mt-2 pa-4">{{ $t('main_content1') }}</h1>
+        </div>
+      </v-img>
+  
+      <!-- Conteúdo principal -->
+      <v-main class="pa-0">
+        <v-container>
+          <div class="px-6 py-4 ml-6">
+            <h1 class="text-h6">{{ $t('heading_termografia') }}</h1>
+            <div style="width: 70%; height: 1px; background-image: linear-gradient(to left, transparent, transparent, #1A237E);" class="mb-4 mt-2"></div>
+            <p class="text-grey-darken-3 text-caption">{{ $t('termografia_description') }}</p>
+          </div>
+  
+          <div class="px-6 py-4">
+            <v-img src="/inspecoes/1.webp" class="rounded"></v-img>
+            <div class="text-grey-darken-3">
+              <p class="text-caption text-md-body-1 my-2">{{ $t('termografia_technique') }}</p>
+              <p class="text-caption text-md-body-1 my-2">{{ $t('termografia_market') }}</p>
+              <p class="text-caption text-md-body-1 my-2">{{ $t('termografia_norms') }}</p>
+              <p class="text-caption text-md-body-1 my-2">{{ $t('termografia_trust') }}</p>
             </div>
-        </v-img>
-
-        
-        <v-main class="pa-0">
+          </div>
+        </v-container>
+  
+        <!-- Seção de análise de vibração -->
+        <div class="bg-grey-lighten-2 px-6 py-2">
+          <v-container>
+            <div class="ml-6 py-2">
+              <div class="mt-4 text-h6 font-weight-bold">{{ $t('heading_vibration_analysis') }}</div>
+              <div style="width: 60%; height: 1px; background-image: linear-gradient(to left, transparent, transparent, #1A237E);" class="my-6"></div>
+              <p class="text-caption font-weight-light">{{ $t('vibration_analysis_description') }}</p>
+            </div>
             <div>
-                <v-container>
-                    <div class="px-6 py-4 ml-6">
-                        <h1 class="text-h6">DESVENDANDO O INVISÍVEL COM A <span class="font-weight-bold tex-h4">TERMOGRAFIA</span></h1>
-                        <div 
-                        style="width: 70%; height: 1px; background-image: linear-gradient(to left, transparent, transparent,#1A237E);" 
-                        class="mb-4 mt-2"></div>
-                        <p class="text-grey-darken-3 text-caption">Evitar falhas e acidentes em instalações industriais é mais do que uma prioridade; é
-                        uma necessidade. Na Pred Engenharia, oferecemos serviços especializados de
-                        inspeção utilizando a tecnologia de termografia para proporcionar uma visão clara
-                        do invisível.</p>
-                    </div>
-
-                    <div class="px-6 py-4">
-                        <v-img src="/inspecoes/1.webp" class="rounded "></v-img>
-                        <div class="text-grey-darken-3 ">
-                            <p class="text-caption text-md-body-1 my-2"><span class="font-weight-bold">A termografia é uma técnica poderosa que nos permite mapear corpos ou regiões,
-                            identificando variações de temperatura que podem indicar potenciais problemas.</span> 
-                            Essa abordagem não apenas previne falhas elétricas, mecânicas e de materiais, mas
-                            também melhora significativamente a eficiência operacional e a segurança.
-                            </p>
-                            <p class="text-caption  text-md-body-1 my-2">Atuamos no mercado de Termografia Industrial desde 1993, fornecendo serviços e
-                            produtos de alta qualidade em todo o Brasil e em outros países. Nossa equipe utiliza
-                            tecnologias de ponta para realizar inspeções, análises e gerenciamentos
-                            termográficos, atendendo às mais diversas necessidades da indústria</p>
-                            <p class="text-caption  text-md-body-1 my-2">Nossos serviços não apenas atendem às recomendações da norma técnica ABNT
-                            NBR 15763.2009, mas também são valorizados por empresas seguradoras, que
-                            reconhecem a termografia como uma prática essencial para avaliações de risco
-                            precisas.</p>
-                            <p class="text-caption text-md-body-1 my-2">Confie na Pred Engenharia para garantir a integridade e o desempenho dos seus
-                            equipamentos e instalações industriais. Agende uma inspeção termográfica hoje
-                            mesmo e descubra uma nova perspectiva para a segurança e eficiência da sua
-                            operação.
-                            </p>
-                        </div>
-                    </div>
-                </v-container>
-
-                <!--outra seção-->
-                <div class="bg-grey-lighten-2  px-6 py-2" >
-                    <v-container>
-                        <div class="ml-6 py-2" >
-                            <div class=" mt-4 text-h6 font-weight-bold ">
-                                ANÁLISE DE VIBRAÇÃO: ANTECIPANDO ANOMALIAS PARA MANUTENÇÃO
-                                PREVENTIVA
-                            </div>
-                            <div    
-                            style="width: 60%; height: 1px; background-image: linear-gradient(to left, transparent, transparent,#1A237E);" 
-                            class="my-6 "></div>
-                            <p class=" text-caption   font-weight-light">Na Pred Engenharia, oferecemos serviços de análise de vibração, uma técnica
-                            fundamental para a detecção precoce e diagnóstico de anomalias em componentes
-                            móveis de equipamentos industriais.</p>
-                        </div>
-                        <div >
-                            <v-img src="/inspecoes/2.webp" class="rounded my-6 mx-auto" max-width="600"></v-img>
-                            <div>
-                                <p class="text-caption text-md-body-1 my-4">Utilizando a condição operacional real do equipamento, nossa análise de vibração é
-                                baseada em medições periódicas ou contínuas de parâmetros significativos. Isso
-                                nos permite antecipar falhas, evitar paradas inesperadas e substituições
-                                desnecessárias de peças, garantindo assim a eficiência e a produtividade contínuas
-                                da sua operação</p>
-                            
-                            </div>
-                        
-                        </div>
-                    </v-container>
-                </div>
-                
-                   <!--outra seção-->
-                <v-container>
-                    <div class="bg-white px-6" >
-                        <div class="ml-6 py-4">
-                            <h1 class="text-h6 font-weight-bold">INSPEÇÃO POR ULTRASSOM: DETECÇÃO PRECISA DE DEFEITOS MECÂNICOS
-                            E VAZAMENTOS
-                            </h1>
-                            <div 
-                            style="width: 100%; height: 1px; background-image: linear-gradient(to left, transparent, transparent,#1A237E);" 
-                            class="my-6"></div>
-                            <p class="text-grey-darken-3 text-caption text-medium-emphasis font-weight-light">A inspeção por ultrassom é outra ferramenta valiosa que oferecemos para garantir a
-                            integridade dos seus equipamentos e instalações industriais.
-                            </p>
-                        </div>
-                        <div >
-                            <v-img src="/inspecoes/3.webp" class="rounded "></v-img>
-                            <p class="text-caption text-md-body-1 my-4">Essa técnica permite a detecção e análise de defeitos mecânicos em máquinas de
-                            baixa rotação, instalações elétricas, vazamentos de fluidos gasosos e muito mais.
-                            Ao detectar sons produzidos por operações mecânicas, emissões elétricas e fluxo
-                            de fluidos, a inspeção por ultrassom é capaz de encontrar falhas em praticamente
-                            todos os equipamentos industriais.</p>
-                            <p class="text-caption text-md-body-1 my-4">Confie na Pred Engenharia para manter seus equipamentos em perfeitas condições
-                            de funcionamento e evitar problemas antes mesmo que eles ocorram. Agende uma
-                            análise de vibração ou inspeção por ultrassom hoje mesmo e proteja o futuro da sua
-                            operação.</p>
-                            
-                        </div>
-                    </div>
-
-                    <div class="px-6 py-4" >
-                        <div class="ml-6 py-4">
-                            <h1 class="text-h6 font-weight-bold">INSPEÇÃO SENSITIVA: UM COMPLEMENTO ESSENCIAL PARA A
-                            MANUTENÇÃO PREDITIVA</h1>
-                            <div 
-                            style="width: 100%; height: 1px; background-image: linear-gradient(to left, transparent, transparent,#1A237E);" 
-                            class="my-6"></div>
-                            <p class="text-grey-darken-3 text-caption text-medium-emphasis font-weight-light">Na Pred Engenharia, entendemos que integrar inspeções sensitivas às preditivas
-                            resulta em insights valiosos e resultados de alto valor agregado para a manutenção
-                            industrial.</p>
-                        </div>
-                        <div >
-                            <v-img src="/inspecoes/4.webp" class="rounded "></v-img>
-                            <p class="text-caption text-md-body-1 my-4">A inspeção sensitiva abrange uma ampla gama de modos de falhas em
-                            equipamentos e instalações industriais. Embora se baseie principalmente nos
-                            sentidos naturais do inspetor, é surpreendentemente eficaz na detecção e
-                            diagnóstico de anomalias em estágios medianos de degradação.</p>
-                            <p class="text-caption text-md-body-1 my-4">Através da utilização de tecnologia de computação móvel, nossas inspeções
-                            sensitivas são elevadas a um novo patamar de inteligência e rastreabilidade. Isso
-                            significa que cada inspeção não apenas identifica problemas potenciais, mas
-                            também fornece dados precisos e acionáveis para guiar ações corretivas e
-                            preventivas.</p>
-                            <p class="text-caption text-md-body-1 my-4">Confie na Pred Engenharia para fornecer soluções completas de inspeção,
-                            integrando tanto abordagens sensitivas quanto preditivas para garantir a integridade
-                            e o desempenho contínuos dos seus ativos industriais. Entre em contato conosco
-                            hoje mesmo para saber mais sobre como podemos ajudar a impulsionar a eficiência
-                            e confiabilidade da sua operação.</p>
-                        </div>
-                        
-                    </div>
-                </v-container>
-                 <FormularioFormulariopages />
-
+              <v-img src="/inspecoes/2.webp" class="rounded my-6 mx-auto" max-width="600"></v-img>
+              <div>
+                <p class="text-caption text-md-body-1 my-4">{{ $t('vibration_analysis_technique') }}</p>
+              </div>
             </div>
-        </v-main>
-
+          </v-container>
+        </div>
+  
+        <!-- Seção de inspeção por ultrassom -->
+        <v-container>
+          <div class="bg-white px-6">
+            <div class="ml-6 py-4">
+              <h1 class="text-h6 font-weight-bold">{{ $t('heading_ultrasound_inspection') }}</h1>
+              <div style="width: 100%; height: 1px; background-image: linear-gradient(to left, transparent, transparent, #1A237E);" class="my-6"></div>
+              <p class="text-grey-darken-3 text-caption text-medium-emphasis font-weight-light">{{ $t('ultrasound_inspection_description') }}</p>
+            </div>
+            <div>
+              <v-img src="/inspecoes/3.webp" class="rounded"></v-img>
+              <p class="text-caption text-md-body-1 my-4">{{ $t('ultrasound_inspection_technique') }}</p>
+              <p class="text-caption text-md-body-1 my-4">{{ $t('ultrasound_inspection_trust') }}</p>
+            </div>
+          </div>
+  
+          <!-- Seção de inspeção sensitiva -->
+          <div class="px-6 py-4">
+            <div class="ml-6 py-4">
+              <h1 class="text-h6 font-weight-bold">{{ $t('heading_sensitive_inspection') }}</h1>
+              <div style="width: 100%; height: 1px; background-image: linear-gradient(to left, transparent, transparent, #1A237E);" class="my-6"></div>
+              <p class="text-grey-darken-3 text-caption text-medium-emphasis font-weight-light">{{ $t('sensitive_inspection_description') }}</p>
+            </div>
+            <div>
+              <v-img src="/inspecoes/4.webp" class="rounded"></v-img>
+              <p class="text-caption text-md-body-1 my-4">{{ $t('sensitive_inspection_technique') }}</p>
+              <p class="text-caption text-md-body-1 my-4">{{ $t('sensitive_inspection_advanced_technology') }}</p>
+              <p class="text-caption text-md-body-1 my-4">{{ $t('sensitive_inspection_trust') }}</p>
+            </div>
+          </div>
+        </v-container>
+  
+        <!-- Componente de formulário -->
+        <FormularioFormulariopages />
+      </v-main>
     </div>
-</template>
+  </template>
+  
