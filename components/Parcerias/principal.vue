4c76b2aad55469b2b4793e09ac6b62a89550be1c
Yanick Santos <yanickdev@outlook.com>
2024-07-13 16:42:34 -0300

    acessibilidade parceiro

--- components/Parcerias/principal.vue
+++ components/Parcerias/principal.vue
@@ -1,11 +1,19 @@
 <template>
     <div class="mb-2 bg-white">
-        <v-container class="w-100 ">
-            <v-row class="w-100 ">
-                <v-col cols="6" md="3"><v-img src="/parceiro/1.webp" ></v-img></v-col>
-                <v-col cols="6" md="3"><v-img src="/parceiro/2.webp"></v-img></v-col>
-                <v-col cols="6" md="3"><v-img src="/parceiro/3.webp"></v-img></v-col>
-                <v-col cols="6" md="3"><v-img src="/parceiro/4.webp"></v-img></v-col>
+        <v-container class="w-100">
+            <v-row class="w-100">
+                <v-col cols="6" md="3">
+                    <v-img src="/parceiro/1.webp" alt="Parceiro 1"></v-img>
+                </v-col>
+                <v-col cols="6" md="3">
+                    <v-img src="/parceiro/2.webp" alt="Parceiro 2"></v-img>
+                </v-col>
+                <v-col cols="6" md="3">
+                    <v-img src="/parceiro/3.webp" alt="Parceiro 3"></v-img>
+                </v-col>
+                <v-col cols="6" md="3">
+                    <v-img src="/parceiro/4.webp" alt="Parceiro 4"></v-img>
+                </v-col>
             </v-row>
         </v-container>
     </div>
