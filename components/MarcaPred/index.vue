1e53d03f1aa34f874283c54b32cdf87b41bd0da2
Yanick Santos <yanickdev@outlook.com>
2024-07-13 17:04:37 -0300

    acessibilidade marcas

--- components/MarcaPred/index.vue
+++ components/MarcaPred/index.vue
@@ -1,31 +1,63 @@
 <template>
-    <div class="d-none d-md-block">
-                <v-img src="/predwhitedesf2.webp" width="600" 
-                        style="position: absolute; top: 800px; left: 40%;  height: 200px; z-index: 100;"
-                ></v-img>
-
-                <v-img src="/predreddesf.webp" width="600" 
-                        style="position: absolute; top: 1810px; right: -1%;  height: 200px;"
-                ></v-img>
-
-                <v-img src="/predwhitedesf.webp" width="80%" cover class="d-none d-lg-block"
-                        style="position: absolute; top: 3710px; left: 5%; z-index: 14;  "
-                ></v-img>
-                
-                <v-img src="/predwhite.webp" width="100" height="500" class="d-none d-lg-block"
-                        style="position: absolute; top: 2250px; left: -2.7%; z-index: 1;  "
-                ></v-img>
-                
-                <v-img src="/predwhite.webp" width="100" height="500" class="d-none d-lg-block"
-                        style="position: absolute; top: 2550px; right:-3.1%; z-index: 1;  "
-                ></v-img>
-
-                <v-img src="/predwhite.webp" width="100" height="500" class="d-none d-lg-block"
-                        style="position: absolute; top: 3150px; left:-2.5%; z-index: 1;  "
-                ></v-img>
-
-                <v-img src="/predblue.webp" width="400" height="1220"  class="d-none d-lg-block"
-                        style="position: absolute; top: 4150px; right: -7%; z-index: 1;  "
-                ></v-img>
-    </div>
+        <div class="d-none d-md-block">
+          <v-img
+            src="/predwhitedesf2.webp"
+            width="600"
+            style="position: absolute; top: 800px; left: 40%; height: 200px; z-index: 100;"
+            alt="Descrição da imagem 1"
+          ></v-img>
+      
+          <v-img
+            src="/predreddesf.webp"
+            width="600"
+            style="position: absolute; top: 1810px; right: -1%; height: 200px;"
+            alt="Descrição da imagem 2"
+          ></v-img>
+      
+          <v-img
+            src="/predwhitedesf.webp"
+            width="80%"
+            cover
+            class="d-none d-lg-block"
+            style="position: absolute; top: 3710px; left: 5%; z-index: 14;"
+            alt="Descrição da imagem 3"
+          ></v-img>
+      
+          <v-img
+            src="/predwhite.webp"
+            width="100"
+            height="500"
+            class="d-none d-lg-block"
+            style="position: absolute; top: 2250px; left: -2.7%; z-index: 1;"
+            alt="Descrição da imagem 4"
+          ></v-img>
+      
+          <v-img
+            src="/predwhite.webp"
+            width="100"
+            height="500"
+            class="d-none d-lg-block"
+            style="position: absolute; top: 2550px; right: -3.1%; z-index: 1;"
+            alt="Descrição da imagem 5"
+          ></v-img>
+      
+          <v-img
+            src="/predwhite.webp"
+            width="100"
+            height="500"
+            class="d-none d-lg-block"
+            style="position: absolute; top: 3150px; left: -2.5%; z-index: 1;"
+            alt="Descrição da imagem 6"
+          ></v-img>
+      
+          <v-img
+            src="/predblue.webp"
+            width="400"
+            height="1220"
+            class="d-none d-lg-block"
+            style="position: absolute; top: 4150px; right: -7%; z-index: 1;"
+            alt="Descrição da imagem 7"
+          ></v-img>
+        </div>
 </template>
+      
